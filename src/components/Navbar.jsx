7b962b3770240e294fed01732982a6c8367888b4
bascelik01 <vukvuksae@gmail.com>
2025-11-20 13:23:26 +0100

    Initial components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return (
+        <ul>
+            <li> Home </li>
+            <li>Devices</li>
+            <li>Documentation</li>
+            <li>Forum</li>
+            <li>Profile</li>
+        </ul>
+    );
+}
+
+export default Navbar;
